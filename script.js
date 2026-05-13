6a50626f3eda0287afef8cfcabe686fa6ce4b32d
xenesis-rahdan <noysiqin@gmail.com>
2026-05-13 22:22:07 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
   // CHANGE TO SWITCH PAGES
   // Options: "open" / "soldout" / "comingsoon"
-  const STATUS        = "comingsoon";
-  const CATEGORY_NAME = "Presale";
+  const STATUS        = "open";
+  const CATEGORY_NAME = "Reguler";
   const TICKET_PRICE  = 25000;
   const MAX_QTY       = 1;
 
